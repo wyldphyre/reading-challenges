b0500652c08730b78fae02357c8b5468e29ada31
Craig Reynolds <craig.reynolds@health.telstra.com>
2025-01-30 13:32:48 +0800

    An example of how to make an SDK for the API, using the Refit package

---
+++ ReadingChallenges/Challenges.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Challenges.Api.Sdk;
+using Challenges.Contracts.Requests;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using System.Text.Json;
+
+//var challengesApi = RestService.For<IChallengesApi>("https://localhost:7192");
+
+var services = new ServiceCollection();
+
+services.AddRefitClient<IChallengesApi>()
+    .ConfigureHttpClient(x =>
+        x.BaseAddress = new Uri("https://localhost:7192"));
+
+var provider = services.BuildServiceProvider();
+
+var challengesApi = provider.GetRequiredService<IChallengesApi>();
+
+//var challenges = await challengesApi.GetAllAsync();
+
+var newChallenge = await challengesApi.CreateAsync(new CreateChallengeRequest
+{
+    Name = "testing 2",
+    Target = 100,
+    Completed = 0,
+    Year = 2025
+});
+
+Console.WriteLine(JsonSerializer.Serialize(newChallenge));
+
+var updatedChallenge = await challengesApi.UpdateAsync(newChallenge.Id.ToString(), new UpdateChallengeRequest
+{
+    Name = "testing 2",
+    Target = 150,
+    Completed = 0,
+    Year = 2025
+});
+
+
+Console.WriteLine(JsonSerializer.Serialize(updatedChallenge));
+
+Console.ReadLine();
---
+++ ReadingChallenges/Challenges.Api.Sdk/IChallengesApi.cs
@@ -0,0 +1,26 @@
+﻿using Challenges.Contracts.Requests;
+using Challenges.Contracts.Responses;
+using Refit;
+
+namespace Challenges.Api.Sdk;
+
+/// <summary>
+/// This interface is used by the Refit package to generate the SDK implementation
+/// </summary>
+public interface IChallengesApi
+{
+    [Post(ApiEndpoints.Challenges.Create)]
+    Task<ChallengeResponse> CreateAsync(CreateChallengeRequest request);
+
+    [Get(ApiEndpoints.Challenges.Get)]
+    Task<ChallengeResponse> GetChallengeAsync(string id);
+
+    [Get(ApiEndpoints.Challenges.GetAll)]
+    Task<ChallengesResponse> GetAllAsync();
+
+    [Put(ApiEndpoints.Challenges.Update)]
+    Task<ChallengeResponse> UpdateAsync(string id, UpdateChallengeRequest request);
+
+    [Delete(ApiEndpoints.Challenges.Delete)]
+    Task<bool> DeleteAsync(string id);
+}
